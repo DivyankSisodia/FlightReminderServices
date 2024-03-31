1767b6da5cb56151e661557be69e6f35e2aa3f57
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-03-31 14:48:42 +0530

    cretae notificationTicket model

--- src/index.js
+++ src/index.js
@@ -3,26 +3,31 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/server-config');
 
-const  sendBasicMail  = require('./service/email-service')
+const sendBasicMail = require('./service/email-service')
 
-const setupAndStart = ()=>{
+const cron = require('node-cron');
+
+const setupAndStart = () => {
     const app = express();
 
 
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.listen(PORT, ()=>{
+    app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
 
-        sendBasicMail(
-            'hyderabadfinancedaily@gmail.com',
-            'dhruvsisodia13@gmail.com',
-            'This is a testing email',
-            'Hey, How are you! This is a test email from Flight Reminder Service.',
-        )
+        // sendBasicMail(
+        //     'hyderabadfinancedaily@gmail.com',
+        //     'dhruvsisodia13@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, How are you! This is a test email from Flight Reminder Service.',
+        // )
+
+        cron.schedule('*/1 * * * *', () => {
+            console.log('Running a task every one minutes');
+        })
     });
 }
 
 setupAndStart();
- 
